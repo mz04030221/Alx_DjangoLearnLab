9261d8f5680579d436cd702f1f99351f020877a8
Mohamed Z. <mz04030221@gmail.com>
2025-07-14 12:55:11 +0100

    Implementing Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# "class Author(models.Model)", "return self.name"
+# "class Book(models.Model)", "title", "author"
+# "class Librarian(models.Model)", "library"
