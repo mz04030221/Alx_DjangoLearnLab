f964553c7f1184c640ebcf30d2a96a3cc1cea1db
Mohamed Z. <mz04030221@gmail.com>
2025-07-17 12:49:04 +0100

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 # "class Author(models.Model)", "return self.name"
 # "class Book(models.Model)", "title", "author"
 # "class Librarian(models.Model)", "library"
+# "class UserProfile(models.Model):", "Admin", "Member"
---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,2 @@
+# from .views import list_books, LibraryDetailView, path
+# ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
