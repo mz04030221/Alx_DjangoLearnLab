aa75eaeb2d7b99d13a13a17ba08511ed9de399d3
Mohamed Z. <mz04030221@gmail.com>
2025-07-20 10:13:07 +0100

    update 10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,9 +18,9 @@ urlpatterns = [
     path("books/", views.list_books, name="list_books"),
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
     # Role-based views
-    path("admin-view/", views.admin_view, name="admin_view"),
-    path("librarian-view/", views.librarian_view, name="librarian_view"),
-    path("member-view/", views.member_view, name="member_view"),
+    path("admin/", views.admin_view, name="admin_view"),
+    path("librarian/", views.librarian_view, name="librarian_view"),
+    path("member/", views.member_view, name="member_view"),
 ]
 
 # "from .views import list_books"
