3f62312bc4229bc1d4e6f0af378f56f3ed683e0f
Mohamed Z. <mz04030221@gmail.com>
2025-07-26 21:29:23 +0100

    update 6

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,85 +4,90 @@ from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseForbidden
 from .models import Book
-from .forms import BookForm  
+from .forms import BookForm
+
 
 # Book List View - Requires can_view permission
 @login_required
-@permission_required('bookshelf.can_view', raise_exception=True)
+@permission_required("bookshelf.can_view", raise_exception=True)
 def book_list(request):
     """
     Display list of all books. Requires 'can_view' permission.
     """
     books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+    return render(request, "bookshelf/book_list.html", {"books": books})
+
 
 # Book Detail View - Requires can_view permission
 @login_required
-@permission_required('bookshelf.can_view', raise_exception=True)
+@permission_required("bookshelf.can_view", raise_exception=True)
 def book_detail(request, book_id):
     """
     Display details of a specific book. Requires 'can_view' permission.
     """
     book = get_object_or_404(Book, id=book_id)
-    return render(request, 'bookshelf/book_detail.html', {'book': book})
+    return render(request, "bookshelf/book_detail.html", {"book": book})
+
 
 # Book Create View - Requires can_create permission
 @login_required
-@permission_required('bookshelf.can_create', raise_exception=True)
+@permission_required("bookshelf.can_create", raise_exception=True)
 def book_create(request):
     """
     Create a new book. Requires 'can_create' permission.
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Book created successfully!')
-            return redirect('book_list')
+            messages.success(request, "Book created successfully!")
+            return redirect("book_list")
     else:
         form = BookForm()
-    return render(request, 'bookshelf/book_form.html', {
-        'form': form, 
-        'title': 'Create Book',
-        'action': 'Create'
-    })
+    return render(
+        request,
+        "bookshelf/book_form.html",
+        {"form": form, "title": "Create Book", "action": "Create"},
+    )
+
 
 # Book Edit View - Requires can_edit permission
 @login_required
-@permission_required('bookshelf.can_edit', raise_exception=True)
+@permission_required("bookshelf.can_edit", raise_exception=True)
 def book_edit(request, book_id):
     """
     Edit an existing book. Requires 'can_edit' permission.
     """
     book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = BookForm(request.POST, request.FILES, instance=book)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Book updated successfully!')
-            return redirect('book_detail', book_id=book.id)
+            messages.success(request, "Book updated successfully!")
+            return redirect("book_detail", book_id=book.id)
     else:
         form = BookForm(instance=book)
-    return render(request, 'bookshelf/book_form.html', {
-        'form': form, 
-        'title': 'Edit Book',
-        'action': 'Update',
-        'book': book
-    })
+    return render(
+        request,
+        "bookshelf/book_form.html",
+        {"form": form, "title": "Edit Book", "action": "Update", "book": book},
+    )
+
 
 # Book Delete View - Requires can_delete permission
 @login_required
-@permission_required('bookshelf.can_delete', raise_exception=True)
+@permission_required("bookshelf.can_delete", raise_exception=True)
 def book_delete(request, book_id):
     """
     Delete a book. Requires 'can_delete' permission.
     """
     book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         book.delete()
-        messages.success(request, 'Book deleted successfully!')
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+        messages.success(request, "Book deleted successfully!")
+        return redirect("book_list")
+    return render(request, "bookshelf/book_confirm_delete.html", {"book": book})
+
 
 # Helper view to check user permissions (for testing)
 @login_required
@@ -92,12 +97,17 @@ def user_permissions(request):
     """
     user = request.user
     permissions = {
-        'can_view': user.has_perm('bookshelf.can_view'),
-        'can_create': user.has_perm('bookshelf.can_create'),
-        'can_edit': user.has_perm('bookshelf.can_edit'),
-        'can_delete': user.has_perm('bookshelf.can_delete'),
+        "can_view": user.has_perm("bookshelf.can_view"),
+        "can_create": user.has_perm("bookshelf.can_create"),
+        "can_edit": user.has_perm("bookshelf.can_edit"),
+        "can_delete": user.has_perm("bookshelf.can_delete"),
     }
-    return render(request, 'bookshelf/user_permissions.html', {
-        'user': user,
-        'permissions': permissions
-    })
+    return render(
+        request,
+        "bookshelf/user_permissions.html",
+        {"user": user, "permissions": permissions},
+    )
+
+
+# "from .forms import ExampleForm"
+
